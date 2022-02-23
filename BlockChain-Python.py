6cc3b485ad9540b95fb65780df103f4f2858aab1
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-02-23 16:19:46 +0500

    Pyhton Blockchain

--- BlockChain-Python.py
+++ BlockChain-Python.py
@@ -1,8 +1,10 @@
 import datetime
 import hashlib
 import json
-from urllib import request
+from uuid import uuid4
 from urllib.parse import urlparse
+from flask import Flask, jsonify, request
+import requests
 
 class Block:
     def __init__(self,index,timeStamp,data,hash,previousHash):
@@ -86,3 +88,84 @@ class Block:
                 self.chain = longest_chain
                 return True
         return False
+    
+    app = Flask(__name__)
+    @app.route('/mine_block', methods = ['GET'])
+    def mine_block():
+        previous_block = blockchain.get_previous_block() #return last or tail block
+        previous_proof = previous_block['proof'] # here tail block nonce will be stored in Previous_proof variable
+        proof = blockchain.proof_of_work(previous_proof) #call of POW will return golden nonce
+        previous_hash = blockchain.hash(previous_block) # calculate hash of last block and store in previous_hash of new block
+        blockchain.add_transaction(sender = node_address, receiver = 'Hadelin', amount = 1)
+        block = blockchain.create_block(proof, previous_hash) #add block in chain
+        response = {'message': 'Congratulations, you just mined a block!',
+                    'index': block['index'],
+                    'timestamp': block['timestamp'],
+                    'proof': block['proof'],
+                    'previous_hash': block['previous_hash'],
+                    'transactions': block['transactions']}
+        return jsonify(response), 200
+
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,'length': len(blockchain.chain)}
+    return jsonify(response), 200   
+
+# Checking if the Blockchain is valid
+@app.route('/is_valid', methods = ['GET'])
+def is_valid():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'All good. The Blockchain is valid.'}
+    else:
+        response = {'message': 'Houston, we have a problem. The Blockchain is not valid.'}
+        return jsonify(response), 200
+    
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods = ['POST'])
+def add_transaction():
+    json = request.get_json() # get Post method parameters in JOSN format
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all(key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+# Connecting new nodes
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json() # get addresses of nodes from post request
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node) # add node URLs in node set
+    response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one.','actual_chain': blockchain.chain}
+    return jsonify(response), 200
+
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods = ['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced by the longest one.',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one.','actual_chain': blockchain.chain}
+    return jsonify(response), 200
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5001)
