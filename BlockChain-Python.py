f08fa056ce92759ee704e0316c018eee4f61cf36
Ahmed Tariq <ahmed.tm097@gmail.com>
2022-02-23 15:31:05 +0500

    Pyhton Blockchain

---
+++ BlockChain-Python.py
@@ -0,0 +1,40 @@
+import datetime
+import hashlib
+
+class Block:
+    def __init__(self,index,timeStamp,data,hash,previousHash):
+        self.timeStamp=timeStamp
+        self.data=data
+        self.previousHash=previousHash
+        self.hash=hash
+
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
+        self.create_block(proof = 1, previous_hash = '0')
+        self.nodes = set()
+    
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                'timestamp': str(datetime.datetime.now()),
+                'proof': proof,
+                'previous_hash': previous_hash,
+                'transactions': self.transactions}
+        self.transactions = []
+        self.chain.append(block)
+        return block
+
+    def get_previous_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
